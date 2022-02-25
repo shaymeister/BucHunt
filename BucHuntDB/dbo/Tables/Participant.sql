e7d41d5a41574211528c8c750b35882084b4b411
Carlos Ortiz <ortizc@etsu.edu>
2022-02-24 23:33:24 -0500

    Made Participants Database and made the respective models

---
+++ BucHuntDB/dbo/Tables/Participant.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Participant]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [AccessCode] INT NOT NULL, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [Email] NVARCHAR(MAX) NOT NULL, 
+    [PhoneNumber] NCHAR(10) NOT NULL
+)
---
+++ DataLibrary/DBLogic/ParticipantProcessor.cs
@@ -0,0 +1,31 @@
+﻿using DataLibrary.DataAccess;
+using DataLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLibrary.DBLogic
+{
+    public static class ParticipantProcessor
+    {
+        public static void CreateParticipant(int accesscode, string firstname, string lastname,
+            string emailaddress, int phonenumber)
+        {
+            ParticipantModel data = new ParticipantModel
+            {
+                AccessCode = accesscode,
+                FirstName = firstname,
+                LastName = lastname,
+                Email = emailaddress,
+                PhoneNumber = phonenumber
+            };
+
+            string sql = @"insert into dbo.Participant (AccessCode, FirstName, LastName, Email, PhoneNumber)
+                        values (@AccessCode, @FirstName, @LastName, @Email, @PhoneNumber);";
+
+            SqlDataAccess.SaveData(sql, data);
+        }
+    }
+}
