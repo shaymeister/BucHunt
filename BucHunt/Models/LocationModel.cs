c1ced0d403cac54ba5a6f07265a7d4197a918296
Christian Livengood <LIVENGOODC@ETSU.EDU>
2022-02-28 10:46:21 -0500

    Made a LocationModel

---
+++ BucHunt/Models/LocationModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BucHunt.Models
+{
+    public class LocationModel
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
