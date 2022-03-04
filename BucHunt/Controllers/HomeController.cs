b2ce6f19595aa02ac7137888cb298388318491eb
Carlos Ortiz <ortizc@etsu.edu>
2022-03-04 11:11:02 -0500

    Added validation to sign up page and added email/phone to work
    Added Logic to make the signup page check for semi valid information.
    Also makes sure they enter everything before they can successfully sign up

--- BucHunt/Controllers/HomeController.cs
+++ BucHunt/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -123,4 +124,60 @@ namespace BucHunt.Controllers
             SendEmail(EmailAddress);
         }
     }
+=======
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BucHunt.Controllers;
+using BucHunt.Models;
+
+namespace BucHunt.Controllers
+{
+    public class HomeController : Controller
+    {
+        EmailProcess email = new EmailProcess();
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            ViewBag.Title = "Sign Up";
+
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SignUp(ParticipantModel participant)
+        {
+            if(ModelState.IsValid)
+            {
+                email.SendEmail(participant.Email);
+                email.SendTexts(participant.PhoneNumber, participant.provider);
+                ViewBag.Title = "Successful Sign Up";
+                return RedirectToAction("SuccessfulSignUp", "Home");
+            }
+            
+
+            return View();
+        }
+        public ActionResult SuccessfulSignUp()
+        {
+            ViewBag.Title = "Successful Sign Up";
+
+            return View();
+        }
+        public ActionResult dummySignUp()
+        {
+            ViewBag.Title = "Dummy Sign Up";
+
+            return View();
+        }
+    }
+>>>>>>> Stashed changes
 }
